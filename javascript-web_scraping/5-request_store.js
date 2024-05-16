cc3ffd09dc6ecda254dc26c3fc0693cd401b2a9d
Juan C Lopez <juancalpz@yahoo.com>
2024-05-16 19:24:41 -0400

    UPDATE: semistandard validation

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -5,8 +5,8 @@ const fs = require('fs');
 
 // Check if both arguments are provided
 if (process.argv.length !== 4) {
-    console.error("Usage: node script.js <URL> <output_file>");
-    process.exit(1);
+  console.error("Usage: node script.js <URL> <output_file>");
+  process.exit(1);
 }
 
 const url = process.argv[2];
@@ -14,21 +14,20 @@ const filePath = process.argv[3];
 
 // Make the request
 request(url, function (error, response, body) {
-    if (error) {
-        console.error('Error:', error);
-        process.exit(1);
+  if (error) {
+    console.error('Error:', error);
+    process.exit(1);
+  }
+  if (response.statusCode !== 200) {
+    console.error('Request failed with status code:', response.statusCode);
+    process.exit(1);
+  }
+  // Write the response body to a file
+  fs.writeFile(filePath, body, { encoding: 'utf-8' }, function (err) {
+    if (err) {
+      console.error('Error writing file:', err);
+      process.exit(1);
     }
-    if (response.statusCode !== 200) {
-        console.error('Request failed with status code:', response.statusCode);
-        process.exit(1);
-    }
-
-    // Write the response body to a file
-    fs.writeFile(filePath, body, { encoding: 'utf-8' }, function (err) {
-        if (err) {
-            console.error('Error writing file:', err);
-            process.exit(1);
-        }
-        // No need to print file saved message here
-    });
+    // No need to print file saved message here
+  });
 });

0dc925a0ae80f2b90adbc3ea26090c78a23bceb0
Juan C Lopez <juancalpz@yahoo.com>
2024-05-16 19:19:33 -0400

    CREATE: New file 5-request_store.js

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+// Check if both arguments are provided
+if (process.argv.length !== 4) {
+    console.error("Usage: node script.js <URL> <output_file>");
+    process.exit(1);
+}
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+// Make the request
+request(url, function (error, response, body) {
+    if (error) {
+        console.error('Error:', error);
+        process.exit(1);
+    }
+    if (response.statusCode !== 200) {
+        console.error('Request failed with status code:', response.statusCode);
+        process.exit(1);
+    }
+
+    // Write the response body to a file
+    fs.writeFile(filePath, body, { encoding: 'utf-8' }, function (err) {
+        if (err) {
+            console.error('Error writing file:', err);
+            process.exit(1);
+        }
+        console.log('File saved successfully:', filePath);
+    });
+});
